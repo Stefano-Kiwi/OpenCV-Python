02f8bbe967780da151e5a97b4f500e37d068cf1e
Stefano-Kiwi <stefanobudon@gmail.com>
2022-10-29 21:35:45 -0300

    Corrección ejercicio 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -2,25 +2,23 @@
 # Agregar contorno de color verde a ambas señales viales.
 # Agregar texto de color verde con la leyenda “Alerta al conductor”.
 import cv2
-import numpy as np
 
 imagen = cv2.imread("imagenes/ruta.jpg")
-
-imagenHSV = cv2.cvtColor(imagen,cv2.COLOR_BGR2HSV)  #Convierto la imagen de BGR a HSV
-redBajo1 = np.array([0,0,0], np.uint8) 
-redBajo2 = np.array([8,255,255], np.uint8)
-redAlto1 = np.array([8,255,255], np.uint8)
-redAlto2 = np.array([179,255,255], np.uint8)
-
-maskRed1 = cv2.inRange(imagenHSV, redBajo1, redAlto1)   #Imagen binaria, blanco = encontro el color
-maskRed2 = cv2.inRange(imagenHSV, redBajo2, redAlto2)   #Negro = no está ese color presente
-
-maskRed = cv2.add(maskRed1,maskRed2)    ## Tenemos como resultado una imagen binaria
-maskRedvis = cv2.bitwise_and(imagen,imagen, mask= maskRed)
-#La recorto
+imagenGris = cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)   
+bordes = cv2.Canny(imagenGris,400,895)          #Aproximación más próxima a lo que deseo obtener de la imagen (las señales)
+bordes = cv2.dilate(bordes, None, iterations=1)
+bordes = cv2.erode(bordes, None, iterations=1)
 cv2.imshow("original",imagen)
-cv2.imshow("maskRed",maskRed)
-cv2.imshow("maskRedvis",maskRedvis)
+cv2.imshow("Bordes detectados!",bordes)
+
+contornos, jerarquia = cv2.findContours(bordes,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)   
+
+cv2.drawContours(imagen,contornos,-1, (0,255,0),2)
+
+texto = "Contornos encontrados: "+str(len(contornos))
+cv2.putText(imagen, texto, (10,20), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 1)
+cv2.putText(imagen, "Alerta al conductor", (60,215), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 1)
+cv2.imshow("Objetos encontrados",imagen)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
